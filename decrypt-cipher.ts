009d9763a5627e9351e25f99039341570d069fa0
Simon <sguillochon@ippon.fr>
2022-04-29 20:25:53 +0200

    extract table from file

--- decrypt-cipher.ts
+++ decrypt-cipher.ts
@@ -1,35 +1,8 @@
+import table  from "./vigenere-table";
+
 const args = require('minimist')(process.argv.slice(2));
 const key: string = args.key;
 const message: string = args.message;
-
-const table = {
- "A": 'abcdefghijklmnopqrstuvwxyz',
- "B": 'bcdefghijklmnopqrstuvwxyza',
- "C": 'cdefghijklmnopqrstuvwxyzab',
- "D": 'defghijklmnopqrstuvwxyzabc',
- "E": 'efghijklmnopqrstuvwxyzabcd',
- "F": 'fghijklmnopqrstuvwxyzabcde',
- "G": 'ghijklmnopqrstuvwxyzabcdef',
- "H": 'hijklmnopqrstuvwxyzabcdefg',
- "I": 'ijklmnopqrstuvwxyzabcdefgh',
- "J": 'jklmnopqrstuvwxyzabcdefghi',
- "K": 'klmnopqrstuvwxyzabcdefghij',
- "L": 'lmnopqrstuvwxyzabcdefghijk',
- "M": 'mnopqrstuvwxyzabcdefghijkl',
- "N": 'nopqrstuvwxyzabcdefghijklm',
- "O": 'opqrstuvwxyzabcdefghijklmn',
- "P": 'pqrstuvwxyzabcdefghijklmno',
- "Q": 'qrstuvwxyzabcdefghijklmnop',
- "R": 'rstuvwxyzabcdefghijklmnopq',
- "S": 'stuvwxyzabcdefghijklmnopqr',
- "T": 'tuvwxyzabcdefghijklmnopqrs',
- "U": 'uvwxyzabcdefghijklmnopqrst',
- "V": 'vwxyzabcdefghijklmnopqrstu',
- "W": 'wxyzabcdefghijklmnopqrstuv',
- "X": 'xyzabcdefghijklmnopqrstuvw',
- "Y": 'yzabcdefghijklmnopqrstuvwx',
- "Z": 'zabcdefghijklmnopqrstuvwxy',
-}
 
 console.log(key);
 console.log(message);
